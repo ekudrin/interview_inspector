de841b2114afb907934f6cfc16b6d5d288546ead
ekudrin <ekudrin@gear-games.com>
2025-12-22 16:02:02 +0300

    add pyttsx3 и озвучку вопросов

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import random
-
+import pyttsx3
 from questions import questions
 
 
@@ -9,6 +9,13 @@ def get_random_question(category: str) -> str:
     questions_list = questions.get(category)
     return random.choice(questions_list)
 
+def play_question(question: str) -> str:
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.say(question)
+    engine.runAndWait()
+
 
 if __name__ == "__main__":
     while True:
@@ -16,5 +23,7 @@ if __name__ == "__main__":
         category = input("Статистика \n SQL \n Python \n Data Science \n Machine Learning \n (Exit для выхода) \n")
         if category == "Exit":
             break
-        print(get_random_question(category))
+        question  = get_random_question(category)
+        print(question)
+        play_question(question)
         input("(Enter для вывода следующего вопроса) \n")
